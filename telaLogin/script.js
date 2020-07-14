1d01339db4fb453911927378a3df42dfbc97f615
brcandido <62360477+brcandido@users.noreply.github.com>
2020-07-14 19:06:47 -0300

    Add files via upload

--- telaLogin/script.js
+++ telaLogin/script.js
@@ -13,21 +13,23 @@ function validacao() {
     for (i=0; i<armz.length; i++){
         if (armz[i].nome == username.value && armz[i].senha == password.value){
             window.alert('Login efetuado com sucesso!')
-            break
+            
         }
+        else if (armz[i].nome == username.value && armz[i].senha != password.value){
+            window.alert('Senha incorreta!')            
+            username.value = null
+            password.value = null
+        }
+        else if (armz[i].nome != username.value && armz[i].senha == password.value){
+            window.alert('Nome de usuário incorreto!')
+            username.value = null
+            password.value = null
+            
+        }
+        
         else{
-            if (armz[i].nome == username.value){
-                window.alert('Senha incorreta!')
-                break
-            }
-            else if(armz[i].senha == password.value){
-                window.alert('Nome de usuário incorreto!')
-                break
-            }
-            else{
-                window.alert('Nome de usuário e senha incorretos!')
-                break
-            }
+            continue;
         }
     }
 }
+
